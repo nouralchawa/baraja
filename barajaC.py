a7cd0d76a85a8ae202787614e721baaf63f52e44
Nour <nouralchawa@example.com>
2020-11-12 22:10:03 +0100

    Metodo Repartir en clase Baraja

--- barajaC.py
+++ barajaC.py
@@ -9,3 +9,20 @@ class Baraja():
 
     def barajar(self):
         baraja.barajar(self.mazacote)
+
+
+    def repartir(self, num_jugadores, num_cartas):
+        jugadores = []
+        for i in range(num_jugadores):
+            jugadores.append([])
+
+        if num_jugadores*num_cartas > len(self.mazacote):
+            print ("No hay suficientes cartas")
+        
+        for carta in range(num_cartas):
+            for jugador in range(num_jugadores):
+                #carta = self.mazacote.pop(0)
+                #jugadores[jugador].append(carta)
+                jugadores[jugador].append(self.mazacote.pop(0))
+
+        return jugadores
--- main.py
+++ main.py
@@ -16,5 +16,8 @@ numeros = ['A','2','3','4','5','6','7','S','C','R']
 miBaraja = barajaC.Baraja(palos, numeros)
 
 print (miBaraja.mazacote)
+
+print (miBaraja.repartir(3, 5))
+
 miBaraja.barajar()
 print (miBaraja.mazacote)
